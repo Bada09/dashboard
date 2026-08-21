0e54d22483cabe35c0aac8a631add0fec8cb928d
Bada09 <badadias@gmail.com>
2026-08-21 12:54:14 -0300

    Auto-update Fairmont dashboard (21/08/2026 12:54)

--- users_data.js
+++ users_data.js
@@ -81,8 +81,8 @@ const users = [
         "lqaScore": 42.6,
         "languages": [
             "FR",
-            "PT",
-            "EN"
+            "EN",
+            "PT"
         ],
         "improvement": {
             "pt": "Reforçar padrões LQA",
@@ -956,8 +956,8 @@ const users = [
         "name": "Jullie Anjos",
         "lqaScore": 60.0,
         "languages": [
-            "EN",
             "FR",
+            "EN",
             "PT"
         ],
         "improvement": {
@@ -1260,8 +1260,8 @@ const users = [
         "lqaScore": 74.7,
         "languages": [
             "FR",
-            "PT",
-            "EN"
+            "EN",
+            "PT"
         ],
         "improvement": {
             "pt": "Reforçar padrões LQA",
@@ -1716,8 +1716,8 @@ const users = [
         "lqaScore": 52.0,
         "languages": [
             "FR",
-            "PT",
-            "EN"
+            "EN",
+            "PT"
         ],
         "improvement": {
             "pt": "Reforçar padrões LQA",
